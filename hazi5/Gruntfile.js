aa5deeeb03e20584264538ee5fb8a4a60b7bf1f4
Balazs Meszaros <bmeszaros91@gmail.com>
2013-03-17 14:37:00 +0100

    5. hazi modositas

--- hazi5/Gruntfile.js
+++ hazi5/Gruntfile.js
@@ -2,14 +2,15 @@
 	module.exports = function(grunt) {
 		grunt.initConfig({
 			pkg: grunt.file.readJSON('package.json');
-			build: {
-		        tasks : 'requirejs'
-		    },
 		    requirejs : {
-		    	
+		    	options : {
+		    		baseUrl: "./",
+		    		dir: "./build",
+		    		optimize: "uglify"
+		    	}
 		    },
 			jshint: {
-				files: ['src/lastfm_search.js', 'test/**/*.js'],
+				files: ['src/lastfm_search.js', 'test/**.js'],
 				options: {
 					globals: {
 						jQuery: true,
@@ -18,16 +19,17 @@
 				}
 			},
 		    qunit: {
-			  files: ['test/**/*.html']
+			  files: ['test/**.html']
 			},
 		    watch : {
-		    	files : ['/**/*.js', '/**/*.css', '/**/*.html'],
-		    	tasks: 'build'
+		    	files : ['**.{js,css,html}'],
+		    	tasks: 'requirejs'
 		    },
 		});
 		grunt.loadNpmTasks('grunt-contrib-jshint');
 		grunt.loadNpmTasks('grunt-contrib-qunit');
 		grunt.loadNpmTasks('grunt-contrib-watch');
+		grunt.loadNpmTasks('grunt-requirejs');
 
 		grunt.registerTask('default', 'build jshint qunit');
 		grunt.registerTask('test', 'jshint qunit');
